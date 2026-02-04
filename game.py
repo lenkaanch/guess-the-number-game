a6ca3cb1b648887cc6f0c3f9cef9b39f54eed66a
Elena Anch <107437613+lenkaanch@users.noreply.github.com>
2026-02-05 00:20:58 +0300

    add guess checking logic

--- game.py
+++ game.py
@@ -4,3 +4,7 @@ secret_number = random.randint(1, 10)
 print("Welcome to Guess the Number!")
 print("I am thinking of a number between 1 and 10.")
 guess = input("Enter your guess: ")
+if int(guess) == secret_number:
+    print("Correct! You guessed the number.")
+else:
+    print("Wrong guess.")
